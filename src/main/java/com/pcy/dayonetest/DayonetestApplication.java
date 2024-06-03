3edeb759bd52d4ccbfba529f1b7f656093687fc7
Park, Chan-Yu <pcy4196@kakao.com>
2024-06-04 01:24:21 +0900

    [Mocktio] 간단한 성적 프로그램 구현 - 1

--- src/main/java/com/pcy/dayonetest/DayonetestApplication.java
+++ src/main/java/com/pcy/dayonetest/DayonetestApplication.java
@@ -2,8 +2,10 @@ package com.pcy.dayonetest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories // SpringBoot에서는 꼭 추가를 하지 않아도 괜찮다
 public class DayonetestApplication {
 
 	public static void main(String[] args) {
